99ad39b8983ccc797ec8c1f4e4ae32d564c0d11d
Kennedy <kennedy.lopes@ufersa.edu.br>
2025-10-29 21:34:53 -0300

    Adiciona métodos abs() e angulo() à classe vetor e corrige construtor com ponto

--- vetor.cpp
+++ vetor.cpp
@@ -1,9 +1,11 @@
 #include "ponto.cpp"
+#include <math.h>
 
 class vetor : ponto {
  private:
  public:
   vetor(float x, float y) : ponto(x, y) {}
+  vetor(ponto p) : ponto(p.getX(), p.getY()) {}
   vetor(ponto p1, ponto p2) : ponto() {
     ponto r = p2 - p1;
     setX(r.getX());
@@ -17,7 +19,21 @@ class vetor : ponto {
   vetor operator-(const vetor& outro) {
     return {outro.getX() - this->getX(), outro.getY() - this->getY()};
   }
+
+  float abs() const {
+    return sqrt(getX() * getX() + getY() * getY());
+  }
+
+  float angulo() const {
+    return atan2(getY(), getX());
+  }
+
   void desenhar() const {
     cout << "Vetor (" << getX() << ", " << getY() << ")";
+  }
+
+  friend ostream& operator<<(ostream &os, const vetor &obj) {
+    os << "vetor (" << obj.getX() << ", " << obj.getY() << ")";
+    return os;
   }
 };
