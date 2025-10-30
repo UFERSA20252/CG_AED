0e6062c8a6ef58abee3bc89d91f944bd27f89375
Kennedy <kennedy.lopes@ufersa.edu.br>
2025-10-29 23:44:21 -0300

    Substitui a instância de polígono por triangulo no main e exibe a área do triângulo

--- main.cpp
+++ main.cpp
@@ -2,11 +2,11 @@
 #include "ponto.cpp"
 #include "vetor.cpp"
 #include "poligono.cpp"
+#include "triangulo.cpp"
 
 int main() {
-  ponto *pontos = new ponto[4]{ponto(0,0), ponto(1,0), ponto(1,1), ponto(0,1)};
-  poligono p(pontos, 4);
-  p.desenhar();
-  delete[] pontos;
+  ponto *pontos = new ponto[4]{ponto(0,0), ponto(1,0), ponto(0,1), ponto(1,1)};
+  triangulo t(pontos[0], pontos[1], pontos[2]);
+  cout << "Area: " << t.area() << "\n";
   return 0;
 }
